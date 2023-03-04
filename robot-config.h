c4b98f84e267f17f5ff197fdbb2c1e3fd09a250f
53000Y-Voltage <124475649+53000Y-Voltage@users.noreply.github.com>
2023-03-03 20:44:29 -0800

    Create robot-config.h

---
+++ robot-config.h
@@ -0,0 +1,16 @@
+using namespace vex;
+
+extern brain Brain;
+extern motor flywheel;
+extern drivetrain Drivetrain;
+extern motor_group intake;
+extern motor roller;
+extern digital_out endgameA;
+extern digital_out aa;
+extern digital_out endgameB;
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void);
